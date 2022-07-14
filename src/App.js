29b461879459c76677670f4c5c93e541f2ec7838
Neelam Chavan <neelamchavan410@gmail.com>
2022-07-14 18:36:28 +0530

    created HeaderCard component with basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MyNavbar from './components/MyNavbar';
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
       <MyNavbar />
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderCard from './HeaderCard'
+import SearchForm from './SearchForm'
+
+function Header() {
+    return (
+        <>
+            <HeaderCard />
+            <SearchForm />
+        </>
+    )
+}
+
+export default Header
