3542c8760cdb66f38692e9cb6357775a57f1b412
Neelam Chavan <neelamchavan410@gmail.com>
2022-07-14 19:42:52 +0530

    fetched api data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react'
 import MyNavbar from './components/MyNavbar';
 import Header from './components/Header'
 import EventList from './components/EventList'
 
 function App() {
+  const [eventList, setEventList] = useState([])
+
+  useEffect(() => {
+    getEvents()
+  }, [])
+
+  const getEvents = () => {
+    fetch('https://manage-api.konfhub.com/hosted-events?limit=10')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setEventList(data)
+      });
+  }
+
   return (
     <div>
       <MyNavbar />
