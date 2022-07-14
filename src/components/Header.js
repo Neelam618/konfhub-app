6930f5c3c6363412099d9ae0e1d70bb832ce54f8
Neelam Chavan <neelamchavan410@gmail.com>
2022-07-14 23:08:18 +0530

    header card styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import SearchForm from './SearchForm'
 function Header({ query, setQuery, handleSearch, isPastEvent, setIsPastEvent }) {
     return (
         <>
-            <div className='container' style={{ background: '#FFDBA6', position: 'relative' }}>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div className='container' style={{ background: '#FFDBA6', position: 'relative', borderRadius: 50 }}>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
                     <div>
                         <img src="/images/Events.png" alt="Events" />
-                        <p>
+                        <p style={{ width: 675, padding: '27px 30px' }}>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra
                         </p>
                     </div>
-                    <div>
+                    <div style={{ padding: 41 }}>
                         <img src="/images/events-header.png" alt="Octopus holding a mic" />
                     </div>
                 </div>
