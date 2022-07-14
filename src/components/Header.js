cf5ecd024e73f60f3cabbe7019568783c69586cb
Neelam Chavan <neelamchavan410@gmail.com>
2022-07-14 23:31:29 +0530

    header form styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,15 @@ import SearchForm from './SearchForm'
 function Header({ query, setQuery, handleSearch, isPastEvent, setIsPastEvent }) {
     return (
         <>
-            <div className='container' style={{ background: '#FFDBA6', position: 'relative', borderRadius: 50 }}>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around' }}>
+            <div className='container' style={{ background: '#FFDBA6', position: 'relative', borderRadius: 50, padding: 41 }}>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
                     <div>
                         <img src="/images/Events.png" alt="Events" />
-                        <p style={{ width: 675, padding: '27px 30px' }}>
+                        <p style={{ maxWidth: 675, padding: '27px 30px' }}>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elementum facilisis leo, vel fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui faucibus in ornare quam viverra
                         </p>
                     </div>
-                    <div style={{ padding: 41 }}>
+                    <div style={{ marginBottom: 20 }}>
                         <img src="/images/events-header.png" alt="Octopus holding a mic" />
                     </div>
                 </div>
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,16 +4,16 @@ function SearchForm({ query, handleSearch, isPastEvent, setIsPastEvent }) {
         setIsPastEvent(e.target.value)
     }
     return (
-        <div style={{ width: '597px', position: 'absolute', bottom: '-33px', left: 0, right: 0, margin: '0 auto', background: '#ffffff', display: 'flex', justifyContent: 'space-between', alignItems: 'center', border: '1px solid' }}>
+        <div style={{ width: '597px', position: 'absolute', bottom: '-33px', left: 0, right: 0, margin: '0 auto', background: '#ffffff', display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px 25px', boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.15)', borderRadius: 15 }}>
             <div style={{ width: '100%' }} className="me-3">
-                <label class="form-label">Search</label>
+                <label class="form-label" style={{ fontWeight: 'bold' }}>Search</label>
                 <div class="input-group">
                     <input value={query} onChange={handleSearch} type="text" class="form-control" aria-label="search" />
                 </div>
 
             </div>
             <div style={{ width: '60%' }}>
-                <label class="form-label">Past Events</label>
+                <label class="form-label" style={{ fontWeight: 'bold' }}>Past Events</label>
                 <select value={isPastEvent} onChange={handleSelectChange} class="form-select" aria-label="Select past events">
                     <option selected>Select Type</option>
                     <option value="true">True</option>
