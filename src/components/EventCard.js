91311f65a9e585cd29240f5d0913dd29f059caf3
Neelam Chavan <neelamchavan410@gmail.com>
2022-07-15 12:15:05 +0530

    card content spacing updated

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -5,11 +5,11 @@ function EventCard({ eventItem }) {
         <div class="card" style={{ width: '272px', height: '288px', margin: '10px 0', border: 'none', filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))' }}>
             <img src="/images/card-image.png" class="card-img-top" alt="..." />
             <img src="/images/Union.png" alt="union" width="27" style={{ position: 'absolute', top: 11, left: 14, borderRadius: '4.5px' }} />
-            <div class="card-body">
+            <div class="card-body" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-around' }}>
                 <div style={{ maxWidth: '250px' }}>
                     <h5 class="card-title">{eventItem.name}</h5>
                 </div>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '18px' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                     <div style={{ color: '#141B25', fontSize: '10.8px' }}><img src='/images/u_podium.svg' alt='' className='me-1 align-bottom' />Raddison Blue</div>
                     <div style={{ color: '#141B25', fontSize: '10.8px' }}><img src="/images/info.svg" className='me-1 align-bottom' />{eventItem.is_free ? 'Free' : 'Paid'} | {eventItem.is_virtual ? 'Online' : 'Offline'}</div>
                 </div>
